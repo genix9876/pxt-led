4fafb63b3cca2b062c14e00df46dae7792d9f8a5
genix9876 <neha@thinkgenix.in>
2019-09-10 12:24:57 +0530

    1

--- main.ts
+++ main.ts
@@ -9,11 +9,12 @@ basic.showLeds(`
 
 //% color = #0000f0 icon = "\uf121"
 namespace ledblink{
-    //% blockId=blinkled weight 70 blockGap = 30
-    //% block="blink led at pin %p| digital write %s" 
-    export function blinkled(p: DigitalPin, s: number) {
-        pins.digitalWritePin(p, s);
-    }
+    // //% blockId=blinkled weight 70 blockGap = 30
+    // //% block="blink led at pin %p| digital write %s" 
+    // export function blinkled(p: DigitalPin, s: number) {
+    //     pins.digitalWritePin(p, s);
+    // }
+
     //% blockId=printName weight=60 blockGap=30
     //% block="Enter a string to print %name"
     export function showname(name: string) {
