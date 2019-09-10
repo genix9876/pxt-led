3bff19f33280471f2588e5f50f4e26971c7c06ab
genix9876 <neha@thinkgenix.in>
2019-09-10 16:15:16 +0530

    init

--- main.ts
+++ main.ts
@@ -9,12 +9,6 @@ basic.showLeds(`
 
 //% color = #008000
 namespace ledblink{
-    // //% blockId=blinkled weight 70 blockGap = 30
-    // //% block="blink led at pin %p| digital write %s" 
-    // export function blinkled(p: DigitalPin, s: number) {
-    //     pins.digitalWritePin(p, s);
-    // }
-
     //% blockId=PrintFunction weight=10 blockGap=22
     //% block="value of moisture sensor at pin %name"
     export function showname(name: string) {
